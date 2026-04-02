4156fb95b55c5c8c1858ba2bab2d8fdeca890913
Sruthi S <sruthirajsruthi2005@gmail.com>
2026-04-02 23:22:22 +0530

    Add files via upload

---
+++ alternatedigitswap.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class alternatedigitswap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        char[] arr = n.toCharArray();
+        for (int i = 0; i < arr.length - 1; i += 2) {
+            char temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+        System.out.print(new String(arr));
+        sc.close();
+    }
+}
